edad398cacd32b979c603f5ad9a660cf702c8fb1
Gustavaopere <gustavaopere@gmail.com>
2026-08-27 20:34:36 -0300

    feat: register Arcana network bridge

--- src/main/java/dev/gustavopere/blackarcana/BlackArcanaMod.java
+++ src/main/java/dev/gustavopere/blackarcana/BlackArcanaMod.java
@@ -1,6 +1,7 @@
 package dev.gustavopere.blackarcana;
 
 import com.mojang.logging.LogUtils;
+import dev.gustavopere.blackarcana.network.neoforge.ArcanaNetworkBridge;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
@@ -11,6 +12,7 @@ public final class BlackArcanaMod {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public BlackArcanaMod(IEventBus modEventBus) {
+        modEventBus.addListener(ArcanaNetworkBridge::register);
         LOGGER.info("Black Arcana foundation loaded");
     }
 }
