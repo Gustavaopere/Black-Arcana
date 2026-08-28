31746fb3e4a109489a9d26719d255a07d23c92c7
Gustavaopere <gustavaopere@gmail.com>
2026-08-28 10:16:27 -0300

    feat: add rebindable loadout editor key

--- src/main/java/dev/gustavopere/blackarcana/client/BlackArcanaKeyMappings.java
+++ src/main/java/dev/gustavopere/blackarcana/client/BlackArcanaKeyMappings.java
@@ -11,6 +11,8 @@ public final class BlackArcanaKeyMappings {
             "key.black_arcana.open_radial", GLFW.GLFW_KEY_R, CATEGORY);
     public static final KeyMapping CAST_SELECTED = new KeyMapping(
             "key.black_arcana.cast_selected", GLFW.GLFW_KEY_V, CATEGORY);
+    public static final KeyMapping EDIT_LOADOUT = new KeyMapping(
+            "key.black_arcana.edit_loadout", GLFW.GLFW_KEY_UNKNOWN, CATEGORY);
     public static final KeyMapping[] QUICK_CAST = new KeyMapping[8];
 
     static {
@@ -27,6 +29,7 @@ public final class BlackArcanaKeyMappings {
     public static void register(RegisterKeyMappingsEvent event) {
         event.register(OPEN_RADIAL);
         event.register(CAST_SELECTED);
+        event.register(EDIT_LOADOUT);
         for (KeyMapping mapping : QUICK_CAST) event.register(mapping);
     }
 }
