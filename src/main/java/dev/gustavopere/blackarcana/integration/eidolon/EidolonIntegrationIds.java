1118b990321dcfac656a36749c82695c52fe87fb
Gustavaopere <gustavaopere@gmail.com>
2026-08-29 19:05:10 -0300

    feat: restore Stage 06 Eidolon ritual id

--- src/main/java/dev/gustavopere/blackarcana/integration/eidolon/EidolonIntegrationIds.java
+++ src/main/java/dev/gustavopere/blackarcana/integration/eidolon/EidolonIntegrationIds.java
@@ -8,6 +8,9 @@ public final class EidolonIntegrationIds {
     public static final ResourceLocation PROBE_RITUAL_ID = ResourceLocation.fromNamespaceAndPath(
         "black_arcana",
         "eidolon_integration_probe");
+    public static final ResourceLocation ANCHOR_ATTUNEMENT_RITUAL_ID = ResourceLocation.fromNamespaceAndPath(
+        "black_arcana",
+        "eidolon_anchor_attunement");
 
     private EidolonIntegrationIds() { }
 }
